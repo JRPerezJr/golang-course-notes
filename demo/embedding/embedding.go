8dc38d065e07272188d5f4609a70d7665b58ee88
Juan Perez <jperezdev@vivaldi.net>
2022-02-20 17:04:02 +0900

    completed embedding

---
+++ demo/embedding/embedding.go
@@ -0,0 +1,82 @@
+package main
+
+import "fmt"
+
+const (
+	Small = iota
+	Medium
+	Large
+)
+
+const (
+	Ground = iota
+	Air
+)
+
+type BeltSize int
+type Shipping int
+
+func (b BeltSize) String() string {
+	return []string{"Small", "Medium", "Large"}[b]
+}
+
+func (s Shipping) String() string {
+	return []string{"Ground", "Air"}[s]
+}
+
+type Conveyer interface {
+	Convey() BeltSize
+}
+
+type Shipper interface {
+	Ship() Shipping
+}
+
+type WarehouseAutomator interface {
+	Conveyer
+	Shipper
+}
+
+type SpamMail struct {
+	amount int
+}
+
+func (s SpamMail) String() string {
+	return "Spam Mail"
+}
+
+func (s *SpamMail) Ship() Shipping {
+	return Air
+}
+
+func (s *SpamMail) Convey() BeltSize {
+	return Small
+}
+
+func automate(item WarehouseAutomator) {
+	fmt.Printf("Convey %v on %v conveyor\n", item, item.Convey())
+	fmt.Printf("Ship %v via %v\n", item, item.Ship())
+}
+
+type ToxicWaste struct {
+	weight int
+}
+
+func (t ToxicWaste) String() string {
+	return "Toxic Waste"
+}
+func (t *ToxicWaste) Ship() Shipping {
+	return Ground
+}
+
+func (t *ToxicWaste) Convey() BeltSize {
+	return Large
+}
+
+func main() {
+	mail := SpamMail{4000}
+	automate(&mail)
+
+	toxicMaterial := ToxicWaste{500}
+	automate(&toxicMaterial)
+}
